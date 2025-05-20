19d2a2756cc6ccaa5943fd07893274ced736861b
aiformoodle <omar.system@aiformoodle.com>
2025-05-20 15:03:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { Configuration, OpenAIApi } = require('openai');
-require('dotenv').config();
 
 const app = express();
 app.use(cors());
